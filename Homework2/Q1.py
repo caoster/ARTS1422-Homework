362e64b5213f3a3c4e109bdb16e6eeb66856c896
caoster <caoster@icloud.com>
2023-03-10 22:27:32 +0800

    Update Q2

--- Homework2/Q1.py
+++ Homework2/Q1.py
@@ -6,7 +6,9 @@ boston = np.loadtxt("boston.csv", delimiter=",", skiprows=1)
 
 boston_PCA = decomposition.PCA(n_components=2).fit_transform(boston)
 np.savetxt("boston_PCA.csv", boston_PCA, delimiter=",")
+
 boston_TSNE = manifold.TSNE(n_components=2).fit_transform(boston)
 np.savetxt("boston_TSNE.csv", boston_TSNE, delimiter=",")
+
 boston_MDS = manifold.MDS(n_components=2, normalized_stress="auto").fit_transform(boston)
 np.savetxt("boston_MDS.csv", boston_MDS, delimiter=",")
